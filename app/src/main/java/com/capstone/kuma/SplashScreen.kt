a9fe1b83d65b0a4d7777a9ee946cd17eb0f0830c
riecho14 <riecho14@outlook.com>
2023-05-23 18:41:25 +0700

    fixed some layouts

--- app/src/main/java/com/capstone/kuma/SplashScreen.kt
+++ app/src/main/java/com/capstone/kuma/SplashScreen.kt
@@ -1,15 +1,25 @@
 package com.capstone.kuma
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.WindowInsets
 import com.capstone.kuma.auth.LoginActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
 
         val splashTime: Long = 3000
 
