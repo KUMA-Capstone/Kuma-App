9eb5b069f005756fa99c5edee7ea6c835616802b
riecho14 <riecho14@outlook.com>
2023-06-05 16:12:21 +0700

    The tips feature has been finalized and improved

--- app/src/main/java/com/capstone/kuma/data/adapter/ListTipsAdapter.kt
+++ app/src/main/java/com/capstone/kuma/data/adapter/ListTipsAdapter.kt
@@ -1,5 +1,6 @@
 package com.capstone.kuma.data.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.capstone.kuma.R
 import com.capstone.kuma.data.TipsItem
+import com.capstone.kuma.layout.TipsDetailActivity
 
 class ListTipsAdapter(private val listTips: ArrayList<TipsItem>) : RecyclerView.Adapter<ListTipsAdapter.ListViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -16,11 +18,14 @@ class ListTipsAdapter(private val listTips: ArrayList<TipsItem>) : RecyclerView.
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        val (img, title) = listTips[position]
+        val (img, title, desc) = listTips[position]
         holder.imgPhoto.setImageResource(img)
         holder.tvTitle.text = title
         holder.itemView.setOnClickListener {
-
+            val intent = Intent(holder.itemView.context, TipsDetailActivity::class.java)
+            intent.putExtra("title", title)
+            intent.putExtra("desc", desc)
+            holder.itemView.context.startActivity(intent)
         }
     }
 
